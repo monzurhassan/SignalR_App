07bf3a6c5dba008e38c877752ec206886c512809
monzurhasan <monzur.recom@gmail.com>
2024-10-03 23:55:10 +0600

    Add project files.

---
+++ SignalRApp/Services/Hubs/ConnectedHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp.Services.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ConnectedHub : Hub
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ConnectedHub(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var username = _httpContextAccessor.HttpContext.Session.GetString("username");
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                ConnectedUsers.myConnectedUsers.Add(username);
+                Clients.All.SendAsync("UpdateUsers", ConnectedUsers.myConnectedUsers);
+            }
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var username = _httpContextAccessor.HttpContext.Session.GetString("username");
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                ConnectedUsers.myConnectedUsers.Remove(username);
+                Clients.All.SendAsync("UpdateUsers", ConnectedUsers.myConnectedUsers);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            var username = _httpContextAccessor.HttpContext.Session.GetString("username");
+
+            if (!string.IsNullOrEmpty(username))
+            {
+               await Clients.All.SendAsync("ReceiveMessage", username, message);
+            }
+        }
+    }
+
+}
---
+++ SignalRApp/wwwroot/js/signalr/clientHub.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/ConnectedHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    li.textContent = `${user} says: ${message}`;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
+
+
+connection.on("UpdateUsers", function (userList) {
+    var usersListElement = document.getElementById("usersList");
+    usersListElement.innerHTML = ""; 
+
+    userList.forEach(function (user) {
+        var li = document.createElement("li");
+        li.textContent = user;
+        usersListElement.appendChild(li);
+    });
+});
